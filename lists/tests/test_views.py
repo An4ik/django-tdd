44e88c582316b25774561ac5bdd50d3a98b31592
An4ik <akhmetov.anuar09@gmail.com>
2018-09-26 11:51:07 +0600

    separated unit tests for view following convention: 1 Unit test checks only one thing

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,27 +8,29 @@ from ..views import home_page
 
 class HomePageTest(TestCase):
 
+    def setUp(self):
+        self.csrf_client = Client()
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        csrf_client = Client()
-        response = csrf_client.get('/')
+        response = self.csrf_client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_can_save_POST_request(self):
-        csrf_client = Client()
-        response = csrf_client.post('/', data={'new_item_text': 'A new list item'})
+        self.csrf_client.post('/', data={'new_item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_home_page_redirect_after_POST_request(self):
+        response = self.csrf_client.post('/', data={'new_item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
     def test_home_page_only_saves_item_when_necessary(self):
-        csrf_client = Client()
-        csrf_client.post('/', data={'new_item_text': ''})
+        self.csrf_client.post('/', data={'new_item_text': ''})
         self.assertEqual(Item.objects.count(), 0)
